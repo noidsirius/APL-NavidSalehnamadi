b1e1586d3f1e64b23761c77ccc8b46f5ef98eb8d
noid <navid.saleh.n@gmail.com>
2018-10-02 14:05:09 -0700

    Pass the test

--- Week1/week1.cpp
+++ Week1/week1.cpp
@@ -15,6 +15,8 @@ bool isBlank(string word){
 
 set<string> stop_words;
 
+vector <pair<string, int> > term_frequencies;
+
 void import_stop_words(){
 	ifstream stop_words_file;
 	stop_words_file.open("../stop_words.txt");
@@ -30,7 +32,7 @@ void import_stop_words(){
 string remove_extra_chars(string word){
 	string new_word = "";
 	for(int i=0; i< word.length(); i++)
-		if(isalpha(word[i]) || isspace(word[i]) || word[i] == '\'')
+		if(isalpha(word[i]) || isspace(word[i]) )//|| word[i] == '\'')
 			new_word += tolower(word[i]);
 		else
 			new_word += ' ';
@@ -52,6 +54,39 @@ bool sortbysec(const pair<string,int> &a, const pair<string,int> &b)
     return (a.second > b.second); 
 } 
 
+string getWholeText(string file_name){
+	ifstream text_file(file_name);
+	if(text_file.fail()){
+		cerr <<"This file is not accessible" << endl;
+		exit(1);
+	}
+	string whole_text = "", line;
+	while(getline(text_file, line))
+		whole_text += line + '\n';
+	text_file.close();
+	return whole_text;
+}
+
+void find_frequencies(string whole_text){
+	string word;
+	map<string, int> frequency_words;
+	stringstream ss(whole_text);
+	while(ss >> word){
+		if(stop_words.find(word) != stop_words.end())
+			continue;
+		if(isBlank(word) or word.length() == 1)
+			continue;
+		if(frequency_words.find(word) == frequency_words.end())
+			frequency_words[word] = 1;
+		else
+			frequency_words[word] = frequency_words[word] + 1;
+	}
+	for(map<string,int>::iterator it = frequency_words.begin(); it != frequency_words.end(); it++)
+		term_frequencies.push_back(make_pair(it->first, it->second));
+	sort(term_frequencies.begin(), term_frequencies.end(), sortbysec);
+	
+}
+
 int main(int argc, char *argv[]){
 	if(argc != 2)
 	{
@@ -59,38 +94,9 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 	import_stop_words();
-	ifstream text_file(argv[1]);
-	if(text_file.fail()){
-		cerr <<"This file is not accessible" << endl;
-		return 1;
-	}
-	string whole_text = "", line;
-	while(getline(text_file, line))
-		whole_text += line + '\n';
-	text_file.close();
-	//cout << whole_text << endl;
-	whole_text = refine_text(whole_text);
-	string word;
-	map<string, int> frequency_words;
-	stringstream ss(whole_text);
-	//cout << whole_text << endl;
-	while(ss >> word){
-		if(stop_words.find(word) != stop_words.end())
-			continue;
-		//cout << word << ' ';
-		if(frequency_words.find(word) == frequency_words.end())
-			frequency_words[word] = 1;
-		else{
-			frequency_words[word] = frequency_words[word] + 1;
-		//	cout << word << ' ' << frequency_words[word] << endl;
-		}
-	}
-	//cout << "SS" << endl;
-	vector<pair<string, int> > final_words_frequency;
-	for(map<string,int>::iterator it = frequency_words.begin(); it != frequency_words.end(); it++)
-		final_words_frequency.push_back(make_pair(it->first, it->second));
-	sort(final_words_frequency.begin(), final_words_frequency.end(), sortbysec);
+	string whole_text = refine_text(getWholeText(argv[1]));
+	find_frequencies(whole_text);
 	for(int i=0; i<25; i++)
-		cout << final_words_frequency[i].first << "  -   " << final_words_frequency[i].second << endl;
+		cout << term_frequencies[i].first << "  -   " << term_frequencies[i].second << endl;
 	return 0;
 }
